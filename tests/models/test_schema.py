15d299603229abc38b0830d5ae3123ea1b966201
benoista <alexandre.benoist@epitech.eu>
2026-08-18 22:57:31 +0200

    fix(models): lint fix

--- tests/models/test_schema.py
+++ tests/models/test_schema.py
@@ -1,10 +1,11 @@
-import chunk
 import datetime
-from pydantic import ValidationError
-import pytest
-from src.models.schema import Document, Chunk
 
-# Test if the document is created successfully
+import pytest
+from pydantic import ValidationError
+
+from src.models.schema import Chunk, Document
+
+"""Test if the document is created successfully"""
 def test_document_creation():
     document = Document(
         id="1",
@@ -15,7 +16,7 @@ def test_document_creation():
     )
     assert document.content_hash != ""
 
-# Test if the document raise a exception if create with missing args
+"""Test if the document raise a exception if create with missing args"""
 def test_document_creating_missing_args():
     with pytest.raises(ValidationError):
         document = Document(
@@ -25,7 +26,7 @@ def test_document_creating_missing_args():
             metadata={"author": "John Doe"},
         )
 
-# Test if the metadata (str, str) type is not respected then throw an error
+"""Test if the metadata (str, str) type is not respected then throw an error"""
 def test_document_creation_metadata_types_error():
     with pytest.raises(ValidationError):
         document = Document(
@@ -47,7 +48,7 @@ def test_document_creation_created_types_error():
             metadata={123: "John Doe"},
         )
 
-# Test if the content hash is the same for two documents with the same content
+"""Test if the content hash is the same for two documents with the same content"""
 def test_same_hash_content():
     document = Document(
         id="1",
@@ -66,7 +67,7 @@ def test_same_hash_content():
     assert document.content_hash == document2.content_hash
 
 
-# Test if the content hash is the same for two documents with the same content
+"""Test if the content hash is the same for two documents with the same content"""
 def test_different_hash_content():
     document = Document(
         id="1",
@@ -84,7 +85,7 @@ def test_different_hash_content():
     )
     assert document.content_hash != document2.content_hash
 
-# Test if the Chunk id is created
+"""Test if the Chunk id is created"""
 def test_chunk_creation():
     chunk = Chunk(
         id="1",
